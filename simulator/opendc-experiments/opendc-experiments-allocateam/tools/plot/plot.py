81451fe7e04188ba879799c82118ade2ca0c06bd
Misha Rigot <misharigot@gmail.com>
2020-11-30 19:24:27 +0100

    Added formatting to large tick values in plots.

--- simulator/opendc-experiments/opendc-experiments-allocateam/tools/plot/plot.py
+++ simulator/opendc-experiments/opendc-experiments-allocateam/tools/plot/plot.py
@@ -7,7 +7,39 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import matplotlib.ticker as tick
 
+def reformat_large_tick_values(tick_val, pos):
+    """
+    Turns large tick values (in the billions, millions and thousands) such as 4500 into 4.5K and also appropriately turns 4000 into 4K (no zero after the decimal).
+    """
+    if tick_val >= 1000000000:
+        val = round(tick_val/1000000000, 1)
+        new_tick_format = '{:}B'.format(val)
+    elif tick_val >= 1000000:
+        val = round(tick_val/1000000, 1)
+        new_tick_format = '{:}M'.format(val)
+    elif tick_val >= 1000:
+        val = round(tick_val/1000, 1)
+        new_tick_format = '{:}K'.format(val)
+    elif tick_val < 1000:
+        new_tick_format = round(tick_val, 1)
+    else:
+        new_tick_format = tick_val
+
+    # make new_tick_format into a string value
+    new_tick_format = str(new_tick_format)
+    
+    # code below will keep 4.5M as is but change values such as 4.0M to 4M since that zero after the decimal isn't needed
+    index_of_decimal = new_tick_format.find(".")
+    
+    if index_of_decimal != -1:
+        value_after_decimal = new_tick_format[index_of_decimal+1]
+        if value_after_decimal == "0":
+            # remove the 0 after the decimal point since it's not needed
+            new_tick_format = new_tick_format[0:index_of_decimal] + new_tick_format[index_of_decimal+2:]
+            
+    return new_tick_format
 
 class Plotter():
     OUTPUT_PATH = f"{os.path.dirname(__file__)}/plots/{datetime.now()}"
@@ -42,6 +74,11 @@ class Plotter():
         self.data.plot.barh(x='scenario_id', y=column, )
         plt.ylabel("Allocation policy")
         plt.xlabel(unit)
+        
+        # Set large number formatting to K for thousands and B for billions
+        ax = plt.gca()
+        ax.xaxis.set_major_formatter(tick.FuncFormatter(reformat_large_tick_values))
+
         output_file_path = f'{self.OUTPUT_PATH}/{column}.png'
         plt.savefig(output_file_path)
 
