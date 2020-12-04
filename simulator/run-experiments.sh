8e88735282a1ccc8a503b161d1d717cdadb90987
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2020-12-05 00:31:34 +0100

    Improve experiment execution script to work on macOS

--- simulator/run-experiments.sh
+++ simulator/run-experiments.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
+# Define realpath for platforms that don't support a command line version by default (i.e. macOS)
+command -v realpath > /dev/null 2>&1 || realpath() {
+	[[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 OUTPUT_PATH="$(pwd)/data"
 rm -rf "$OUTPUT_PATH"
 
-./gradlew :opendc-experiments:opendc-experiments-sc20:run --args=\
-"--environment-path \"$(realpath $1)\" --trace-path \"$(realpath $2)\" --portfolio $3 --output \"$OUTPUT_PATH\""
+./gradlew :opendc-experiments:opendc-experiments-sc20:run --args="--environment-path \"$(realpath "$1")\" --trace-path \"$(realpath "$2")\" --portfolio $3 --output \"$OUTPUT_PATH\""
