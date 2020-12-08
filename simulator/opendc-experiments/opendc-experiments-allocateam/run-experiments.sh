8b6c766f6da5644563dbba41aaedd621a7f00c28
Misha Rigot <misharigot@gmail.com>
2020-12-08 15:42:19 +0100

    Delete run-experiments.sh

--- simulator/opendc-experiments/opendc-experiments-allocateam/run-experiments.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-OUTPUT_PATH="$(pwd)/data"
-rm -rf "$OUTPUT_PATH"
-
-./gradlew :opendc-experiments:opendc-experiments-allocateam:run --args=\
-"--environment-path \"$(realpath $1)\" --trace-path \"$(realpath $2)\" --portfolio $3 --output \"$OUTPUT_PATH\""
