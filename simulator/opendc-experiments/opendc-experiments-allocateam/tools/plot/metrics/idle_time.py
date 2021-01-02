2bb99fb250abe80466d208d0ad99bc83c131872d
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2021-01-02 18:16:59 +0100

    Fix idle time issue (where idle time could be negative)

--- simulator/opendc-experiments/opendc-experiments-allocateam/tools/plot/metrics/idle_time.py
+++ simulator/opendc-experiments/opendc-experiments-allocateam/tools/plot/metrics/idle_time.py
@@ -6,10 +6,33 @@ class IdleTimeMetric(Metric):
     def __init__(self, plot, runs):
         super().__init__(plot, runs)
         self.name = "idle_time"
-        self.x_axis_label = "Idle time (in seconds)"
+        self.x_axis_label = "Average idle percentage (per machine)"
 
     def get_data(self, run):
-        run_duration = pd.read_parquet(metric_path("run-duration", run)).run_duration[0]
-        df = pd.read_parquet(metric_path("task-lifecycle", run))
-        df['duration'] = df.finish_time - df.start_time
-        yield ((run_duration - df.duration.sum()) / (run_duration // 1000)) * 100
+        job_df = pd.read_parquet(metric_path("job-lifecycle", run))
+        task_df = pd.read_parquet(metric_path("task-lifecycle", run))
+        task_df["duration"] = task_df.finish_time - task_df.start_time
+
+        server_stats = task_df.groupby("server_id").aggregate({"duration": ["sum"]})
+        server_stats["idle_time"] = job_df.finish_time.max() - server_stats.duration
+
+        sizes = {
+            "small": 32,
+            "medium": 256,
+            "large": 10000
+        }
+        topology_size = sizes[run.topology]
+        unused_servers = topology_size - len(server_stats)
+        unused_server_time = job_df.finish_time.max() * unused_servers
+        if unused_servers > 0:
+            print("Warning: unused servers are not considered for idle time")
+            print("topology: {}, allocation policy: {}, workload: {}".format(
+                run.topology,
+                run.allocation_policy,
+                run.workload_name
+            ))
+            print("Unused servers:", unused_servers)
+            print()
+        idle_time_per_server = (server_stats.idle_time.sum() + unused_server_time) / topology_size
+        print(idle_time_per_server / job_df.finish_time.max() * 100)
+        yield idle_time_per_server / job_df.finish_time.max() * 100
